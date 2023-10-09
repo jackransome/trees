0ac695d37cd36cccf47b82942a10df6cef3dad5e
jackransome <31069425+jackransome@users.noreply.github.com>
2023-10-09 15:43:18 +1100

    Detached camera from cylinders in preparation for collision testing

--- trees/Entity1.cpp
+++ trees/Entity1.cpp
@@ -19,35 +19,10 @@ void Entity1::perLoop(){
 
 	
 	//setting direction according to mouse
-	directionAngle = globals::input.cameraAngle.x + 3.14159265359 / 2;
+	/*directionAngle = globals::input.cameraAngle.x + 3.14159265359 / 2;
 	planeDirection = glm::vec2(std::cos(directionAngle), std::sin(directionAngle));
 	planeDirectionRight = glm::vec2(std::cos(directionAngle + 3.14159265359 / 2), std::sin(directionAngle + 3.14159265359 / 2));
-	//controls
-	speed = 0.01f;
-	if (globals::input.keys.keyCounts["leftShift"] >= 1)
-	{
-		speed = 0.02f;
-	}
-	if (globals::input.keys.keyCounts["w"] >= 1)
-	{
-		planePos += speed *planeDirection;
-	}
-	if (globals::input.keys.keyCounts["a"] >= 1)
-	{
-		planePos += speed * planeDirectionRight;
-	}
-	if (globals::input.keys.keyCounts["s"] >= 1)
-	{
-		planePos -= speed * planeDirection;
-	}
-	if (globals::input.keys.keyCounts["d"] >= 1)
-	{
-		planePos -= speed * planeDirectionRight;
-	}
-	if (globals::input.keys.keyCounts["space"] == 1 && onGround)
-	{
-		hVel = 0.015;
-	}
+
 	height += hVel;
 	hVel -= 0.0005;
 	if (height <= 0) {
@@ -73,22 +48,56 @@ void Entity1::perLoop(){
 
 	rotation.x += 30;
 	rotation.y += 60;
-	rotation.z += 90;
+	rotation.z += 90;*/
 
 	
 
 	//camera
 
-	glm::vec3 forward = segment->getDirectionNormalized();
-	glm::vec3 up = segment->convertPlaneToNormal(planePos.x);
+	glm::vec3 forward = glm::vec3(0,0,1);// segment->getDirectionNormalized();
+	glm::vec3 up = glm::vec3(0, 1, 0);// segment->convertPlaneToNormal(planePos.x);
 	glm::vec3 right = glm::normalize(glm::cross(forward, up));
 	glm::vec3 cameraAngle = globals::input.cameraAngle;
 	glm::vec3 newUp = up;
 
+	glm::mat4 rotationMatrix = glm::rotate(glm::mat4(1.0f), cameraAngle.x, up);
+	glm::vec3 newForward = glm::vec3(rotationMatrix * glm::vec4(forward, 1.0f));
+	glm::vec3 newRight = glm::vec3(rotationMatrix * glm::vec4(right, 1.0f));
+	//controls
+	speed = 0.01f;
+	if (globals::input.keys.keyCounts["leftAlt"] >= 1)
+	{
+		speed = 0.02f;
+	}
+	if (globals::input.keys.keyCounts["w"] >= 1)
+	{
+		position += speed * newForward;
+	}
+	if (globals::input.keys.keyCounts["a"] >= 1)
+	{
+		position -= speed * newRight;
+	}
+	if (globals::input.keys.keyCounts["s"] >= 1)
+	{
+		position -= speed * newForward;
+	}
+	if (globals::input.keys.keyCounts["d"] >= 1)
+	{
+		position += speed * newRight;
+	}
+	if (globals::input.keys.keyCounts["space"] >= 1)
+	{
+		position.y += speed;
+	}
+	if (globals::input.keys.keyCounts["leftShift"] >= 1)
+	{
+		position.y -= speed;
+	}
+
 	// ------ rotating the forward vector
 
 	// rotating forward with y rotation
-	glm::mat4 rotationMatrix = glm::rotate(glm::mat4(1.0f), cameraAngle.y, right);
+	rotationMatrix = glm::rotate(glm::mat4(1.0f), cameraAngle.y, right);
 	glm::vec3 yRotatedForward = glm::vec3(rotationMatrix * glm::vec4(forward, 1.0f));
 
 	// rotating y rotated forward with x rotation
@@ -115,11 +124,12 @@ void Entity1::perLoop(){
 	// Convert the result back to a vec3 and return it
 	newUp = glm::vec3(rotatedVec4);
 
-	position = segment->convertPlaneToRealCoords(planePos) + up*height;
+	//position = segment->convertPlaneToRealCoords(planePos) + up*height;
 	cameraFrom = position + up * 0.2f;
 	cameraTo = position + up * 0.2f + xRotatedForward;
 	cameraUp = newUp;
-	if (!switched) {
+	globals::gfx.setCameraManually(cameraFrom, cameraTo, cameraUp);
+	/*if (!switched) {
 		globals::gfx.setCameraManually(cameraFrom, cameraTo, cameraUp);
 	}
 	else {
@@ -132,7 +142,7 @@ void Entity1::perLoop(){
 			switched = false;
 			switchCounter = 0;
 		}
-	}
+	}*/
 	
 	//std::cout << globals::input.cameraAngle.x << " | " << globals::input.cameraAngle.y << "\n";
 	updateVkObjectState();
@@ -145,7 +155,7 @@ void Entity1::changeSegment(Csegment* _segment){
 	switched = true;
 	segment = _segment;
 	segmentBounds = segment->getPlaneDims();
-	globals::input.cameraAngle = segment->getNewCameraAngle(cameraTo - cameraFrom, cameraUp, planePos.x);
+	//globals::input.cameraAngle = segment->getNewCameraAngle(cameraTo - cameraFrom, cameraUp, planePos.x);
 }
 
 Csegment* Entity1::getSegment()
