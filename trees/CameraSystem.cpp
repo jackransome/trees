0d5e92e58b18ffd6bb704902ae2d056f77dc71f0
jackransome <31069425+jackransome@users.noreply.github.com>
2023-11-10 17:28:40 +1100

    Fixed view angle changing / up vector bug. ECS Refactor is complete

--- trees/CameraSystem.cpp
+++ trees/CameraSystem.cpp
@@ -9,12 +9,12 @@ void CameraSystem::moveWithMouse(Input* input)
 	glm::vec3 cameraAngleDelta = glm::vec3(0, 0, 0);
 
 	// get camera angle delta from mouse input
-	//cameraAngleDelta.y = mouseSensitivity * float(input->windowHeight / 2 - input->mouseYpos);
-	//cameraAngleDelta.x = mouseSensitivity * float(input->windowWidth / 2 - input->mouseXpos);
+	cameraAngleDelta.y = mouseSensitivity * float(input->windowHeight / 2 - input->mouseYpos);
+	cameraAngleDelta.x = mouseSensitivity * float(input->windowWidth / 2 - input->mouseXpos);
 
 	//calculate new right axis
-
-	glm::mat4 rotationMatrix = glm::rotate(glm::mat4(1.0f), cameraAngleDelta.z, forward);
+	glm::mat4 rotationMatrix;
+	rotationMatrix = glm::rotate(glm::mat4(1.0f), cameraAngleDelta.z, forward);
 	up = glm::vec3(rotationMatrix * glm::vec4(up, 1.0f));
 	right = glm::normalize(glm::cross(forward, up));
 
@@ -22,13 +22,15 @@ void CameraSystem::moveWithMouse(Input* input)
 	rotationMatrix = glm::rotate(glm::mat4(1.0f), cameraAngleDelta.y, right);
 	forward = glm::vec3(rotationMatrix * glm::vec4(forward, 1.0f));
 
+	//now rotate the up vector by the y delta around the "right" axis
+	rotationMatrix = glm::rotate(glm::mat4(1.0f), cameraAngleDelta.y, right);
+	up = glm::normalize(glm::vec3(rotationMatrix * glm::vec4(up, 1.0f)));
+
 	//now rotate the forward vector by the x delta around the "up" axis
 	rotationMatrix = glm::rotate(glm::mat4(1.0f), cameraAngleDelta.x, up);
 	forward = glm::normalize(glm::vec3(rotationMatrix * glm::vec4(forward, 1.0f)));
 
-	//now rotate the up vector by the y delta around the "right" axis
-	rotationMatrix = glm::rotate(glm::mat4(1.0f), cameraAngleDelta.y, right);
-	up = glm::normalize(glm::vec3(rotationMatrix * glm::vec4(up, 1.0f)));
+
 
 	from = position;
 	to = position + forward;
